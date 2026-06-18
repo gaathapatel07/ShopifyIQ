b7cdb5f2f29088f02064bb4b22bdbeae0b3eceb7
Gaatha Patel <140440288+gaathapatel07@users.noreply.github.com>
2026-06-18 23:30:46 +0530

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,9 @@
+export interface RetailRow {
+  InvoiceNo: string; StockCode: string; Description: string
+  Quantity: number; InvoiceDate: string; UnitPrice: number
+  CustomerID: number; Country: string
+}
+export interface KPIData {
+  totalRevenue: number; totalOrders: number
+  totalCustomers: number; conversionRate: number
+}
